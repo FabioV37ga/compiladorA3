e3f541112c3c601ae3b4d03611ea9ad301153556
Fábio Veiga <veigabfabio@hotmail.com>
2024-06-14 18:10:22 -0300

    Correção no codigo intermediário (linhas estavam sem aspas)

--- js/compilador/intermediate.js
+++ js/compilador/intermediate.js
@@ -2,7 +2,7 @@ class Intermediate {
     static gerarCodIntermediario() {
         // Função interna: traduz a representação do parser para código intermediário
         function translateToIntermediateCode() {
-            let pseudoCode = JSON.parse(main.finalString)
+            let pseudoCode = JSON.parse(main.parsedString)
 
             let intermediateCode = [];
 
@@ -19,7 +19,7 @@ class Intermediate {
                         break;
 
                     case 'declaracao':
-                        
+
                         let varType = node[1];
                         // Para cada declaração de variável, gera a instrução de declaração no código intermediário
                         node[2].forEach(varDecl => {
@@ -83,17 +83,23 @@ class Intermediate {
                         let varNameAssign = node[1][0];
                         console.log(node)
                         var expression;
-                        if (node[0] && node[0] == 'binop'){
+
+                        if (node[0] && node[0] == 'binop') {
                             expression = `${node[2]} ${node[3][2][0]} ${node[3][1][0]} ${node[3][3][0]}`;
                             console.log("cuzinho")
-                        }else if(node[2][0] == 'binop'){
+                        } else if (node[2][0] == 'binop') {
                             expression = `:= ${node[2][2][0]} ${node[2][1][0]} ${node[2][3][0]}`;
                             console.log("outrumacaca")
 
-                        }else if(node[0] == 'atribuicao'){
+                        } else if (node[0] == 'atribuicao') {
+                            if (node[2][1] == 'TEXTO') {
+                                expression = `:= "${node[2][0]}"`
+                            }else{
+                                expression = `:= ${node[2][0]}`
+                            }
                             console.log("outrum2")
-                            expression = `:= ${node[2][0]}`
-                        }else{
+                        } else {
+                            console.log("avestrue pelasda")
                             expression = `${node[2]} ${node[3][0]}`;
                         }
                         intermediateCode.push(`${varNameAssign} ${expression}`);
--- js/compilador/main.js
+++ js/compilador/main.js
@@ -3,7 +3,8 @@ class main {
     static tokens;  // Armazena os tokens obtidos após a análise léxica
     static semantic;  // Instância para análise semântica
     static parser;  // Instância do parser para análise sintática
-    static finalString;  // Armazena a representação final do código em formato string JSON
+    static parsedString;  // Armazena a representação final do código em formato string JSON
+    static intermediateString;
 
     // Método getCodigo: Responsável por ler a entrada do usuário
     static getCodigo() {
@@ -12,15 +13,46 @@ class main {
         main.tokens = Lexer.getTokens()
         main.semantic = new Semantic()
         main.parser = new Parser(main.tokens)
-        main.finalString = JSON.stringify(main.parser.parse(), null, 2)
+        main.parsedString = JSON.stringify(main.parser.parse(), null, 2)
 
         // Exibe a representação do parser na interface do usuário
-        document.querySelector(".output-parser").value = this.finalString
+        document.querySelector(".output-parser").value = this.parsedString
         // console.log(Intermediate.gerarCodIntermediario())
 
         // Traduz a representação do parser para código intermediário e exibe na interface
-        document.querySelector(".output-intermediate").value = Intermediate.gerarCodIntermediario()
+
+        main.intermediateString = Intermediate.gerarCodIntermediario()
+        document.querySelector(".output-intermediate").value = main.intermediateString
         
-        Intermediate.gerarCodIntermediario()
+        // Intermediate.gerarCodIntermediario()
+
+
+        function downloadTxtFile() {
+            // Conteúdo do arquivo
+            const texto = "Este é o conteúdo do arquivo que será baixado.";
+        
+            // Cria um Blob com o texto
+            const blob = new Blob([texto], { type: 'text/plain' });
+        
+            // Cria um URL temporário
+            const url = URL.createObjectURL(blob);
+        
+            // Cria um elemento de link <a>
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'meuarquivo.txt'; // Nome do arquivo a ser baixado
+        
+            // Adiciona o elemento de link ao documento
+            document.body.appendChild(a);
+        
+            // Simula um clique no elemento para iniciar o download
+            a.click();
+        
+            // Limpa o objeto URL criado
+            URL.revokeObjectURL(url);
+        
+            // Remove o elemento <a> do documento
+            document.body.removeChild(a);
+        }
     }
 }
