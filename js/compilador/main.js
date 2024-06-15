01ac7e4c188080194eff34b2bef2d2a6f238e8e0
Fábio Veiga <veigabfabio@hotmail.com>
2024-06-15 12:08:33 -0300

    1.1.0 - Separação por abas

--- js/compilador/main.js
+++ js/compilador/main.js
@@ -5,6 +5,7 @@ class main {
     static parser;  // Instância do parser para análise sintática
     static parsedString;  // Armazena a representação final do código em formato string JSON
     static intermediateString;
+    static finalString;
 
     // Método getCodigo: Responsável por ler a entrada do usuário
     static getCodigo() {
@@ -17,46 +18,14 @@ class main {
 
         // Exibe a representação do parser na interface do usuário
         document.querySelector(".output-parser").value = this.parsedString
-        // console.log(Intermediate.gerarCodIntermediario())
 
         // Traduz a representação do parser para código intermediário e exibe na interface
-
         main.intermediateString = Intermediate.gerarCodIntermediario()
         document.querySelector(".output-intermediate").value = main.intermediateString
 
-        // Intermediate.gerarCodIntermediario()
-
-        console.log()
-        // eval(Tradutor.traduzirFinal(main.intermediateString.toString()))
+        main.finalString = Tradutor.traduzirFinal(main.intermediateString.toString())
+        document.querySelector(".output-final").value = this.finalString;
 
 
-
-        function downloadTxtFile() {
-            // Conteúdo do arquivo
-            const texto = "Este é o conteúdo do arquivo que será baixado.";
-
-            // Cria um Blob com o texto
-            const blob = new Blob([texto], { type: 'text/plain' });
-
-            // Cria um URL temporário
-            const url = URL.createObjectURL(blob);
-
-            // Cria um elemento de link <a>
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'meuarquivo.txt'; // Nome do arquivo a ser baixado
-
-            // Adiciona o elemento de link ao documento
-            document.body.appendChild(a);
-
-            // Simula um clique no elemento para iniciar o download
-            a.click();
-
-            // Limpa o objeto URL criado
-            URL.revokeObjectURL(url);
-
-            // Remove o elemento <a> do documento
-            document.body.removeChild(a);
-        }
     }
 }
--- js/frontend/editor.js
+++ js/frontend/editor.js
@@ -4,5 +4,29 @@ $(".btn-compilar")[0].addEventListener("click", () => {
 })
 
 $(".btn-exec")[0].addEventListener("click", () => {
-    eval(`${Tradutor.traduzirFinal(main.intermediateString.toString())}`)
+    if (main.finalString)
+        eval(`${main.finalString}`)
 })
+
+
+var tabs = document.querySelectorAll(".secao-direita .tab")
+var outputTextArea = $(".secao-direita .textBox-output")
+
+for (let i = 0; i <= tabs.length - 2; i++) {
+    tabs[i].addEventListener("click", function (e) {
+        for (let ind = 0; ind <= tabs.length - 2; ind++) {
+
+            tabs[ind].classList.add("-off");
+            e.target.classList.remove("-off");
+
+            outputTextArea[ind].classList.add("hidden")
+
+            if (e.target.id)
+                outputTextArea[e.target.id].classList.remove("hidden")
+            else
+                outputTextArea[e.target.parentElement.id].classList.remove("hidden")
+        }
+    })
+}
+
+console.log(outputTextArea)
